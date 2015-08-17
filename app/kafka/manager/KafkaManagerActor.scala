3c3c3496be9c6baf7276578bf521416af0d966b0
Viswa Vutharkar <vvutharkar@ebay.com>
2015-08-17 13:06:11 -0700

    Adding 0.8.2.1 in supported versions
    and also bumping .deb version to 1.2

--- app/kafka/manager/KafkaManagerActor.scala
+++ app/kafka/manager/KafkaManagerActor.scala
@@ -38,7 +38,8 @@ object KafkaVersion {
   val supportedVersions: Map[String,KafkaVersion] = Map(
     "0.8.1.1" -> Kafka_0_8_1_1, 
     "0.8.2-beta" -> Kafka_0_8_2_0,
-    "0.8.2.0" -> Kafka_0_8_2_0)
+    "0.8.2.0" -> Kafka_0_8_2_0,
+    "0.8.2.1" -> Kafka_0_8_2_0)
 
   val formSelectList : IndexedSeq[(String,String)] = supportedVersions.toIndexedSeq.filterNot(_._1.contains("beta")).map(t => (t._1,t._2.toString))
 
--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@
 name := """kafka-manager"""
 
 /* For packaging purposes, -SNAPSHOT MUST contain a digit */
-version := "1.1"
+version := "1.2"
 
 scalaVersion := "2.11.5"
 
