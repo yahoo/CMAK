cvcal/kafka-manager

Add consumer-level information, along with the latest-produced offsets to the topics.
The KafkaStateActor now tracks the following additional information:
- adds a Curator treeCache to cache the consumer information, which includes the names of consumer instances that own each partition, and the offsets at every partition.
- adds cached offsets for the topics. These must be gotten directly from Kafka as they are not stored in Zookeeper, so the call functions by using a simple consumer for every partition. Currently, the frequency of updates to these offsets is hard-coded to every 5 seconds or less frequently.
- configurable in the clusterConfig is the possibility to exclude any consumer that do not have an offsets/ owners/ AND ids/ subdirectory as being "inactive"

The UI now has a consumer list view, which lists all consumers and the topics they consume from, an individual page for each consumer that lists each topic with the cumulative lag and the percentage of partitions that have an owner (if all is well, this should be 100%), a page for every consumed topic that lists the offset, lag, and owner information at every partition for that consumer-topic combination. To the topic views, there is now the most recent offset at every partition and the cumulative log size, as well as links to the consumed state of the topic inside a list of the consumers that consume from this topic.

app/kafka/manager/BrokerViewCacheActor.scala
@@ -33,6 +33,12 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
 
   private[this] var topicDescriptionsOption : Option[TopicDescriptions] = None
 
+  private[this] var topicConsumerMap : Map[String, Iterable[String]] = Map.empty
+
+  private[this] var consumerIdentities : Map[String, ConsumerIdentity] = Map.empty
+
+  private[this] var consumerDescriptionsOption : Option[ConsumerDescriptions] = None
+
   private[this] var brokerListOption : Option[BrokerList] = None
 
   private[this] var brokerMetrics : Map[Int, BrokerMetrics] = Map.empty
@@ -115,6 +121,7 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
         //ask for topic descriptions
         val lastUpdateMillisOption: Option[Long] = topicDescriptionsOption.map(_.lastUpdateMillis)
         context.actorSelection(config.kafkaStateActorPath).tell(KSGetAllTopicDescriptions(lastUpdateMillisOption), self)
+        context.actorSelection(config.kafkaStateActorPath).tell(KSGetAllConsumerDescriptions(lastUpdateMillisOption), self)
         context.actorSelection(config.kafkaStateActorPath).tell(KSGetBrokers, self)
 
       case BVGetViews =>
@@ -134,6 +141,12 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
 
       case BVGetTopicIdentities =>
         sender ! topicIdentities
+
+      case BVGetTopicConsumerMap =>
+        sender ! topicConsumerMap
+
+      case BVGetConsumerIdentities =>
+        sender ! consumerIdentities
 
       case BVUpdateTopicMetricsForBroker(id, metrics) =>
         metrics.foreach {
@@ -168,6 +181,10 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
         topicDescriptionsOption = Some(td)
         updateView()
 
+      case cd: ConsumerDescriptions =>
+        consumerDescriptionsOption = Some(cd)
+        updateView()
+
       case bl: BrokerList =>
         brokerListOption = Some(bl)
         updateView()
@@ -179,6 +196,11 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
   implicit def queue2finitequeue[A](q: Queue[A]): FiniteQueue[A] = new FiniteQueue[A](q)
 
   private[this] def updateView(): Unit = {
+    updateViewForBrokersAndTopics()
+    updateViewsForConsumers()
+  }
+
+  private[this] def updateViewForBrokersAndTopics(): Unit = {
     for {
       brokerList <- brokerListOption
       topicDescriptions <- topicDescriptionsOption
@@ -255,6 +277,20 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
           brokerTopicPartitions.put(
             brokerId, BVView(topicPartitionsMap, config.clusterConfig, brokerMetrics.get(brokerId)))
       }
+    }
+  }
+
+  private[this] def updateViewsForConsumers(): Unit = {
+    for {
+      consumerDescriptions <- consumerDescriptionsOption
+    } {
+      val consumerIdentity : IndexedSeq[ConsumerIdentity] = consumerDescriptions.descriptions.map(
+          ConsumerIdentity.from(_, config.clusterConfig))
+      consumerIdentities = consumerIdentity.map(ci => (ci.consumerGroup, ci)).toMap
+
+      val c2tMap = consumerDescriptions.descriptions.map{cd: ConsumerDescription =>
+        (cd.consumer, cd.topics.keys.toList)}.toMap
+      topicConsumerMap = c2tMap.values.flatten.map(v => (v, c2tMap.keys.filter(c2tMap(_).contains(v)))).toMap
     }
   }
 }

yahoo/CMAK

app/kafka/manager/BrokerViewCacheActor.scala
@@ -34,6 +34,12 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
 
   private[this] var topicDescriptionsOption : Option[TopicDescriptions] = None
 
+  private[this] var topicConsumerMap : Map[String, Iterable[String]] = Map.empty
+
+  private[this] var consumerIdentities : Map[String, ConsumerIdentity] = Map.empty
+
+  private[this] var consumerDescriptionsOption : Option[ConsumerDescriptions] = None
+
   private[this] var brokerListOption : Option[BrokerList] = None
 
   private[this] var brokerMetrics : Map[Int, BrokerMetrics] = Map.empty
@@ -116,6 +122,7 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
         //ask for topic descriptions
         val lastUpdateMillisOption: Option[Long] = topicDescriptionsOption.map(_.lastUpdateMillis)
         context.actorSelection(config.kafkaStateActorPath).tell(KSGetAllTopicDescriptions(lastUpdateMillisOption), self)
+        context.actorSelection(config.kafkaStateActorPath).tell(KSGetAllConsumerDescriptions(lastUpdateMillisOption), self)
         context.actorSelection(config.kafkaStateActorPath).tell(KSGetBrokers, self)
 
       case BVGetViews =>
@@ -135,6 +142,12 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
 
       case BVGetTopicIdentities =>
         sender ! topicIdentities
+
+      case BVGetTopicConsumerMap =>
+        sender ! topicConsumerMap
+
+      case BVGetConsumerIdentities =>
+        sender ! consumerIdentities
 
       case BVUpdateTopicMetricsForBroker(id, metrics) =>
         metrics.foreach {
@@ -169,6 +182,10 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
         topicDescriptionsOption = Some(td)
         updateView()
 
+      case cd: ConsumerDescriptions =>
+        consumerDescriptionsOption = Some(cd)
+        updateView()
+
       case bl: BrokerList =>
         brokerListOption = Some(bl)
         updateView()
@@ -180,6 +197,11 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
   implicit def queue2finitequeue[A](q: Queue[A]): FiniteQueue[A] = new FiniteQueue[A](q)
 
   private[this] def updateView(): Unit = {
+    updateViewForBrokersAndTopics()
+    updateViewsForConsumers()
+  }
+
+  private[this] def updateViewForBrokersAndTopics(): Unit = {
     for {
       brokerList <- brokerListOption
       topicDescriptions <- topicDescriptionsOption
@@ -256,6 +278,20 @@ class BrokerViewCacheActor(config: BrokerViewCacheActorConfig) extends LongRunni
           brokerTopicPartitions.put(
             brokerId, BVView(topicPartitionsMap, config.clusterContext, brokerMetrics.get(brokerId)))
       }
+    }
+  }
+
+  private[this] def updateViewsForConsumers(): Unit = {
+    for {
+      consumerDescriptions <- consumerDescriptionsOption
+    } {
+      val consumerIdentity : IndexedSeq[ConsumerIdentity] = consumerDescriptions.descriptions.map(
+          ConsumerIdentity.from(_, config.clusterContext))
+      consumerIdentities = consumerIdentity.map(ci => (ci.consumerGroup, ci)).toMap
+
+      val c2tMap = consumerDescriptions.descriptions.map{cd: ConsumerDescription =>
+        (cd.consumer, cd.topics.keys.toList)}.toMap
+      topicConsumerMap = c2tMap.values.flatten.map(v => (v, c2tMap.keys.filter(c2tMap(_).contains(v)))).toMap
     }
   }
 }