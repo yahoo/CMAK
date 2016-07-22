c41fed03dc0f19dcf43f2e3770707252bdb7287e
Kelsey Lam <kelseylam@berkeley.edu>
2016-07-22 14:44:04 -0700

    Added routes and methods to KafkaStateCheck to get brokers skew and get clusters lit

--- app/controllers/api/KafkaStateCheck.scala
+++ app/controllers/api/KafkaStateCheck.scala
@@ -1,12 +1,13 @@
 /**
- * Copyright 2015 Yahoo Inc. Licensed under the Apache License, Version 2.0
- * See accompanying LICENSE file.
- */
+  * Copyright 2015 Yahoo Inc. Licensed under the Apache License, Version 2.0
+  * See accompanying LICENSE file.
+  */
 
 package controllers.api
 
 import controllers.KafkaManagerContext
 import features.ApplicationFeatures
+import kafka.manager.model.ActorModel.KMClusterList
 import models.navigation.Menus
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.libs.json._
@@ -14,8 +15,8 @@ import play.api.mvc._
 import scala.concurrent.Future
 
 /**
- * @author jisookim0513
- */
+  * @author jisookim0513
+  */
 
 class KafkaStateCheck (val messagesApi: MessagesApi, val kafkaManagerContext: KafkaManagerContext)
                       (implicit af: ApplicationFeatures, menus: Menus) extends Controller with I18nSupport {
@@ -37,7 +38,42 @@ class KafkaStateCheck (val messagesApi: MessagesApi, val kafkaManagerContext: Ka
     kafkaManager.getTopicList(c).map { errorOrTopicList =>
       errorOrTopicList.fold(
         error => BadRequest(Json.obj("msg" -> error.msg)),
-        topicList => Ok(Json.obj("topics" -> topicList.list.sorted))
+        topicList => Ok(Json.obj("topics" -> topicList.list))
+      )
+    }
+  }
+
+  def clusters(status: Option[String]) = Action.async { implicit request =>
+    kafkaManager.getClusterList.map { errorOrClusterList =>
+      errorOrClusterList.fold(
+        error => BadRequest(Json.obj("msg" -> error.msg)),
+        //                clusterList => Ok(Json.obj("clusters" -> (clusterList.active.map(cc => Map("name" -> cc.name, "status" -> "active")) ++
+        //                  clusterList.pending.map(cc => Map("name" -> cc.name, "status" -> "pending"))).sortBy(_("name"))))
+        clusterList => Ok(makeJson(clusterList, status))
+      )
+    }
+  }
+
+  def makeJson(clusterList: KMClusterList, status: Option[String]) = {
+    val active = clusterList.active.map(cc => Map("name" -> cc.name, "status" -> "active"))
+    val pending = clusterList.pending.map(cc => Map("name" -> cc.name, "status" -> "pending"))
+
+    if (status.isEmpty) {
+      Json.obj("clusters" -> (active ++ pending).sortBy(_("name")))
+    } else if (status.get == "active") {
+      Json.obj("clusters" -> active.sortBy(_("name")))
+    } else if (status.get == "pending") {
+      Json.obj("clusters" -> pending.sortBy(_("name")))
+    } else {
+      Json.obj("option" -> status)
+    }
+  }
+
+  def brokersSkewPercentage(c: String, t:String) = Action.async { implicit request =>
+    kafkaManager.getTopicIdentity(c, t).map { errorOrTopicIdentity =>
+      errorOrTopicIdentity.fold(
+        error => BadRequest(Json.obj("msg" -> error.msg)),
+        topicIdentity => Ok(Json.obj("topic" -> t, "brokersSkewPercentage" -> topicIdentity.brokersSkewPercentage))
       )
     }
   }
