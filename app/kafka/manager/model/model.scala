f4d006d2f8ef1d66a53e5970033f47113d02b670
fholmqvist <fholmqvist@innometrics.com>
2016-08-17 13:34:24 +0200

    Enable Kafka 0.10.0.0 and 0.10.0.1. Seems to work ok, needs proper testing.

--- app/kafka/manager/model/model.scala
+++ app/kafka/manager/model/model.scala
@@ -37,6 +37,12 @@ case object Kafka_0_9_0_0 extends KafkaVersion {
 case object Kafka_0_9_0_1 extends KafkaVersion {
   override def toString = "0.9.0.1"
 }
+case object Kafka_0_10_0_0 extends KafkaVersion {
+  override def toString = "0.10.0.0"
+}
+case object Kafka_0_10_0_1 extends KafkaVersion {
+  override def toString = "0.10.0.1"
+}
 
 object KafkaVersion {
   val supportedVersions: Map[String,KafkaVersion] = Map(
@@ -46,7 +52,9 @@ object KafkaVersion {
     "0.8.2.1" -> Kafka_0_8_2_1,
     "0.8.2.2" -> Kafka_0_8_2_2,
     "0.9.0.0" -> Kafka_0_9_0_0,
-    "0.9.0.1" -> Kafka_0_9_0_1
+    "0.9.0.1" -> Kafka_0_9_0_1,
+    "0.10.0.0" -> Kafka_0_10_0_0,
+    "0.10.0.1" -> Kafka_0_10_0_1
   )
 
   val formSelectList : IndexedSeq[(String,String)] = supportedVersions.toIndexedSeq.filterNot(_._1.contains("beta")).map(t => (t._1,t._2.toString))
