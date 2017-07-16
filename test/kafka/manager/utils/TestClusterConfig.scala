c4430ce153241989c91f2d98150128356c25e416
yuya <life.of.151a@gmail.com>
2017-07-16 18:04:27 +0900

    add serialization/deserialization test for ver 0.10.2.1

--- test/kafka/manager/utils/TestClusterConfig.scala
+++ test/kafka/manager/utils/TestClusterConfig.scala
@@ -110,4 +110,12 @@ class TestClusterConfig extends FunSuite with Matchers {
     assert(deserialize.isSuccess === true)
     assert(cc == deserialize.get)
   }
+
+  test("serialize and deserialize 0.10.2.1") {
+    val cc = ClusterConfig("qa", "0.10.2.1", "localhost:2181", jmxEnabled = false, pollConsumers = true, filterConsumers = true, activeOffsetCacheEnabled = true, jmxUser = None, jmxPass = None, jmxSsl = false, tuning = None)
+    val serialize: String = ClusterConfig.serialize(cc)
+    val deserialize = ClusterConfig.deserialize(serialize)
+    assert(deserialize.isSuccess === true)
+    assert(cc == deserialize.get)
+  }
 }
