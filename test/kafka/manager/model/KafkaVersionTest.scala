fe6de3750e6d8d2cc622b1ebc48bbef77cdc0aea
yuya <life.of.151a@gmail.com>
2017-07-29 00:45:38 +0900

    Create KafkaVersionTest.scala

---
+++ test/kafka/manager/model/KafkaVersionTest.scala
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2017 Yahoo Inc. Licensed under the Apache License, Version 2.0
+ * See accompanying LICENSE file.
+ */
+package kafka.manager.model
+
+import org.scalatest.FunSuite
+
+/**
+  * @author fuji-151a
+  */
+class KafkaVersionTest extends FunSuite {
+
+  test("testFormSelectList") {
+
+  }
+
+}
