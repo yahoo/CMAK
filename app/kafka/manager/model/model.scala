c55f298237ae4f07241410f12b924be140a6d1d6
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2018-12-20 11:29:29 +0100

    Add support for kafka v1.1.1

--- app/kafka/manager/model/model.scala
+++ app/kafka/manager/model/model.scala
@@ -80,6 +80,10 @@ case object Kafka_1_1_0 extends KafkaVersion {
   override def toString = "1.1.0"
 }
 
+case object Kafka_1_1_1 extends KafkaVersion {
+  override def toString = "1.1.1"
+}
+
 object KafkaVersion {
   val supportedVersions: Map[String,KafkaVersion] = Map(
     "0.8.1.1" -> Kafka_0_8_1_1,
@@ -99,7 +103,8 @@ object KafkaVersion {
     "0.11.0.2" -> Kafka_0_11_0_2,
     "1.0.0" -> Kafka_1_0_0,
     "1.0.1" -> Kafka_1_0_1,
-    "1.1.0" -> Kafka_1_1_0
+    "1.1.0" -> Kafka_1_1_0,
+    "1.1.1" -> Kafka_1_1_1
   )
 
   val formSelectList : IndexedSeq[(String,String)] = supportedVersions.toIndexedSeq.filterNot(_._1.contains("beta")).map(t => (t._1,t._2.toString)).sortWith((a, b) => sortVersion(a._1, b._1))
--- test/kafka/manager/utils/TestClusterConfig.scala
+++ test/kafka/manager/utils/TestClusterConfig.scala
@@ -188,4 +188,12 @@ class TestClusterConfig extends FunSuite with Matchers {
     assert(deserialize.isSuccess === true)
     assert(cc == deserialize.get)
   }
+
+  test("serialize and deserialize 1.1.1") {
+    val cc = ClusterConfig("qa", "1.1.1", "localhost:2181", jmxEnabled = false, pollConsumers = true, filterConsumers = true, activeOffsetCacheEnabled = true, jmxUser = None, jmxPass = None, jmxSsl = false, tuning = None, securityProtocol = "PLAINTEXT")
+    val serialize: String = ClusterConfig.serialize(cc)
+    val deserialize = ClusterConfig.deserialize(serialize)
+    assert(deserialize.isSuccess === true)
+    assert(cc == deserialize.get)
+  }
 }
