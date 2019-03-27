53dcff120b89d4f827115a7297da4d5a44730979
Sebastian Spanner <sebastian.spanner@p7s1produktion.de>
2019-03-27 17:12:51 +0100

    Support Kafka 2.2.0

--- app/controllers/Topic.scala
+++ app/controllers/Topic.scala
@@ -67,6 +67,7 @@ class Topic (val messagesApi: MessagesApi, val kafkaManagerContext: KafkaManager
   val kafka_2_0_0_Default = CreateTopic("",1,1,TopicConfigs.configNames(Kafka_2_0_0).map(n => TConfig(n,None)).toList)
   val kafka_2_1_0_Default = CreateTopic("",1,1,TopicConfigs.configNames(Kafka_2_1_0).map(n => TConfig(n,None)).toList)
   val kafka_2_1_1_Default = CreateTopic("",1,1,TopicConfigs.configNames(Kafka_2_1_1).map(n => TConfig(n,None)).toList)
+  val kafka_2_2_0_Default = CreateTopic("",1,1,TopicConfigs.configNames(Kafka_2_2_0).map(n => TConfig(n,None)).toList)
 
 
   val defaultCreateForm = Form(
@@ -167,6 +168,7 @@ class Topic (val messagesApi: MessagesApi, val kafkaManagerContext: KafkaManager
           case Kafka_2_0_0 => (defaultCreateForm.fill(kafka_2_0_0_Default), clusterContext)
           case Kafka_2_1_0 => (defaultCreateForm.fill(kafka_2_1_0_Default), clusterContext)
           case Kafka_2_1_1 => (defaultCreateForm.fill(kafka_2_1_1_Default), clusterContext)
+          case Kafka_2_2_0 => (defaultCreateForm.fill(kafka_2_1_1_Default), clusterContext)
         }
       }
     }
@@ -416,6 +418,7 @@ class Topic (val messagesApi: MessagesApi, val kafkaManagerContext: KafkaManager
           case Kafka_2_0_0 => TopicConfigs.configNames(Kafka_2_0_0).map(n => (n,TConfig(n,None))).toMap
           case Kafka_2_1_0 => TopicConfigs.configNames(Kafka_2_1_0).map(n => (n,TConfig(n,None))).toMap
           case Kafka_2_1_1 => TopicConfigs.configNames(Kafka_2_1_1).map(n => (n,TConfig(n,None))).toMap
+          case Kafka_2_2_0 => TopicConfigs.configNames(Kafka_2_2_0).map(n => (n,TConfig(n,None))).toMap
         }
         val combinedMap = defaultConfigMap ++ ti.config.toMap.map(tpl => tpl._1 -> TConfig(tpl._1,Option(tpl._2)))
         (defaultUpdateConfigForm.fill(UpdateTopicConfig(ti.topic,combinedMap.toList.map(_._2),ti.configReadVersion)),
