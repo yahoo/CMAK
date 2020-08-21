c3b5140937814c5d6716eef7860d951b1bacda8e
VHAISDMelhiT <VHAISDMelhiT@281-LT-MELHIT.vba.va.gov>
2020-08-21 12:01:37 -0500

    Fix cut/paste typo for 2.6.0

--- test/kafka/manager/utils/TestClusterConfig.scala
+++ test/kafka/manager/utils/TestClusterConfig.scala
@@ -293,8 +293,8 @@ class TestClusterConfig extends FunSuite with Matchers {
     assert(cc == deserialize.get)
   }
   
-    test("serialize and deserialize 2.6.1") {
-    val cc = ClusterConfig("qa", "2.6.1", "localhost:2181", jmxEnabled = false, pollConsumers = true, filterConsumers = true, activeOffsetCacheEnabled = true, jmxUser = None, jmxPass = None, jmxSsl = false, tuning = None, securityProtocol = "SASL_PLAINTEXT", saslMechanism = Option("PLAIN"), jaasConfig = Option("blah"))
+    test("serialize and deserialize 2.6.0") {
+    val cc = ClusterConfig("qa", "2.6.0", "localhost:2181", jmxEnabled = false, pollConsumers = true, filterConsumers = true, activeOffsetCacheEnabled = true, jmxUser = None, jmxPass = None, jmxSsl = false, tuning = None, securityProtocol = "SASL_PLAINTEXT", saslMechanism = Option("PLAIN"), jaasConfig = Option("blah"))
     val serialize: String = ClusterConfig.serialize(cc)
     val deserialize = ClusterConfig.deserialize(serialize)
     assert(deserialize.isSuccess === true)
